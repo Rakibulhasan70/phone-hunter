4b974dbcd1c3444866f64dda7b915d3d4179695a
Rakibulhasan70 <rakibulhasansohag5@gmail.com>
2022-03-01 01:37:51 +0600

    second-part

--- index.js
+++ index.js
@@ -20,12 +20,38 @@ const displayPhones = phones => {
         <div class="card mx-auto">
         <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="...">
         <div class="card-body">
-            <h5 class="card-title">Brand Name : ${phone.brand}</h5>
+            <h5 class="card-title">Brand Name : ${phone.data}</h5>
             <p class="card-text">Phone-Name : ${phone.phone_name}</p>
+            <button onclick ="buttonDetails('${phone.slug}')" class="px-2 rounded bg-secondary text-white border-1">Details</button>
         </div>
     </div>
         `
         searchTotal.appendChild(div);
     });
 
+}
+
+
+const buttonDetails = (phoneId) => {
+    console.log(phoneId)
+    const url = ` https://openapi.programming-hero.com/api/phone/${phoneId}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayButtonDetails(data.data))
+}
+
+const displayButtonDetails = phones => {
+    console.log(phones)
+    const singlePhoneDetails = document.getElementById('singlePhoneDetails')
+    singlePhoneDetails.textContent = ''
+    const div = document.createElement('div')
+    div.classList.add('card')
+    div.innerHTML = `
+    <img src="${phones.image}" class="card-img-top  w-25 mx-auto" alt="...">
+    <div class="card-body">
+        <h4 class="card-text">Device-Name : ${phones.name}</h4>
+        <p>${phones.releaseDate ? phones.releaseDate : ''}</p>
+    </div>
+    `
+    singlePhoneDetails.appendChild(div)
 }
